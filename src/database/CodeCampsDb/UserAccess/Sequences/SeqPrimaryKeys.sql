ede9b0ade3aa79c1be0c7d3502ef46cbd0e301ea
Ryan Harris <trharris@auw.com>
2020-01-17 17:06:19 -0800

    Add database project

---
+++ src/database/CodeCampsDb/UserAccess/Sequences/SeqPrimaryKeys.sql
@@ -0,0 +1,5 @@
+﻿CREATE SEQUENCE [UserAccess].[SeqPrimaryKeys]
+    AS BIGINT
+    START WITH 1
+    INCREMENT BY 1;
+
---
+++ src/database/CodeCampsDb/UserAccess/Tables/UserRegistration.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [UserAccess].[UserRegistration] (
+    [Id]               BIGINT           NOT NULL,
+    [RegistrationId]   UNIQUEIDENTIFIER NOT NULL,
+    [Login]            NVARCHAR (64)    NOT NULL,
+    [ConfirmationCode] VARCHAR (16)     NOT NULL,
+    [ExpiryDate]       DATETIME         NOT NULL,
+    [PasswordHash]     NVARCHAR (512)   NOT NULL,
+    [PasswordSalt]     NVARCHAR (128)   NOT NULL,
+    [FirstName]        NVARCHAR (32)    NOT NULL,
+    [LastName]         NVARCHAR (64)    NOT NULL,
+    [Email]            NVARCHAR (128)   NOT NULL,
+    [Status]           VARCHAR (32)     NOT NULL,
+    [CreatedBy]        VARCHAR (32)     CONSTRAINT [UserAccess_UserRegistration_CreatedBy] DEFAULT (suser_sname()) NOT NULL,
+    [CreatedOn]        DATETIME         CONSTRAINT [UserAccess_UserRegistration_CreatedOn] DEFAULT (getdate()) NOT NULL,
+    [UpdatedBy]        VARCHAR (32)     CONSTRAINT [UserAccess_UserRegistration_UpdatedBy] DEFAULT (suser_sname()) NOT NULL,
+    [UpdatedOn]        DATETIME         CONSTRAINT [UserAccess_UserRegistration_UpdatedOn] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [UserAccess_UserRegistration_Id] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
