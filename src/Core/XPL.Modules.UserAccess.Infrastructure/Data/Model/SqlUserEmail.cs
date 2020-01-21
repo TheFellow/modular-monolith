c1fe96ebfa0fb9e68c1f10d95ef8d93f71d6a076
Ryan Harris <trharris@auw.com>
2020-01-21 13:52:59 -0800

    Added new Sql DTOs for the User* tables

---
+++ src/Core/XPL.Modules.UserAccess.Infrastructure/Data/Model/SqlUserEmail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace XPL.Modules.UserAccess.Infrastructure.Data.Model
+{
+    public class SqlUserEmail
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public DateTime StatusDate { get; set; }
+        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTime UpdatedOn { get; set; }
+    }
+}
---
+++ src/Core/XPL.Modules.UserAccess.Infrastructure/Data/Model/SqlUserLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using XPL.Framework.Infrastructure.Persistence;
+
+namespace XPL.Modules.UserAccess.Infrastructure.Data.Model
+{
+    public class SqlUserLogin : ISqlId
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public string PasswordHash { get; set; } = string.Empty;
+        public string PasswordSalt { get; set; } = string.Empty;
+        public DateTime BeginOnUtc { get; set; }
+        public DateTime EndOnUtc { get; set; }
+        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTime UpdatedOn { get; set; }
+    }
+}
