006c1a3f00d63ce8907a472de8c2b9d2a65a80be
Ryan Harris <trharris@auw.com>
2020-01-30 11:31:36 -0800

    Update the services which query the back end to refer to their memebers as queryContext so it's clear that no tracking behavior is happening.

--- src/XPL.Modules.UserAccess.Infrastructure/Emails/EmailUsage.cs
+++ src/XPL.Modules.UserAccess.Infrastructure/Emails/EmailUsage.cs
@@ -11,16 +11,16 @@ namespace XPL.Modules.UserAccess.Infrastructure.Emails
 {
     public class EmailUsage : IEmailUsage
     {
-        private readonly IUserAccessQueryContext _dbContext;
-        public EmailUsage(IUserAccessQueryContext dbContext) => _dbContext = dbContext;
+        private readonly IUserAccessQueryContext _queryContext;
+        public EmailUsage(IUserAccessQueryContext queryContext) => _queryContext = queryContext;
 
         public Option<Login> TryGetLoginForEmail(EmailAddress newEmail) =>
-            _dbContext.Users
+            _queryContext.Users
             .Where(u => u.Emails.Select(e => e.Email).Contains(newEmail.Value))
             .Select(su => su.Login)
             .ToList() // Can't Concat over different DbSets
             .Concat(
-                _dbContext.UserRegistrations
+                _queryContext.UserRegistrations
                 .Where(r => r.Email == newEmail.Value)
                 .Select(r => r.Email))
             .FirstOrNone()
--- src/XPL.Modules.UserAccess.Infrastructure/UserRegistrations/Rules/LoginExists.cs
+++ src/XPL.Modules.UserAccess.Infrastructure/UserRegistrations/Rules/LoginExists.cs
@@ -8,19 +8,19 @@ namespace XPL.Modules.UserAccess.Infrastructure.UserRegistrations.Rules
 {
     public class LoginExists : ILoginExists
     {
-        private readonly IUserAccessQueryContext _dbContext;
+        private readonly IUserAccessQueryContext _queryContext;
 
-        public LoginExists(IUserAccessQueryContext dbContext) => _dbContext = dbContext;
+        public LoginExists(IUserAccessQueryContext queryContext) => _queryContext = queryContext;
 
         bool ILoginExists.LoginExists(Login login)
         {
-            var loginSql = _dbContext
+            var loginSql = _queryContext
                 .UserRegistrations
                 .Where(u => u.Login == login.Value)
                 .Select(l => l.Login)
                 .ToList() // Can't Concat over different DbSets
                 .Concat(
-                    _dbContext.Users
+                    _queryContext.Users
                     .Where(u => u.Login == login.Value)
                     .Select(l => l.Login))
                 .FirstOrNone();
