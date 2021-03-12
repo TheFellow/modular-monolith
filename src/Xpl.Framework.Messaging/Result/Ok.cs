87865bb1292f12e5c9a93cd46173a61e851848e9
Ryan Harris <thomasryanharris@gmail.com>
2021-03-11 23:48:29 -0800

    Split result classes

---
+++ src/Xpl.Framework.Messaging/Result/Ok.cs
@@ -0,0 +1,8 @@
+﻿namespace Xpl.Framework.Messaging.Result
+{
+    public class Ok<T> : Result<T>
+    {
+        public T Value { get; init; }
+        public Ok(T value) => Value = value;
+    }
+}
